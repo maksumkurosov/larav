08557b9e285ef23e303294bb91fb7a62301c9691
maksumkurnosov <m.kurnosv@nltu.lviv.ua>
2018-10-25 14:19:45 +0300

    article page

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -42,7 +42,7 @@
                         role="button" aria-expanded="false">Блог</a>
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="{{route('admin.category.index')}}">Категории</a> </li>
-                            <li><a href="#">Материалы</a> </li>
+                            <li><a href="{{route('admin.article.index')}}">Материалы</a> </li>
                         </ul>
                     </li>
                 </ul>
